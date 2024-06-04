a78ed7a12f4bd708b2801fd188e9bbef2eaf38a6
Lora Khatib <92958149+lkhatib@users.noreply.github.com>
2024-06-04 09:50:16 -0700

    Delete BIRDMAn/diagnosis directory

--- BIRDMAn/diagnosis/normal_dementia/species_amyloid_birdman_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Submit the first script as an array job and capture its job ID
-ARRAY_JOB_ID=$(sbatch --parsable --array=1-10 species_birdman.sh)
-
-# Submit the second script with a dependency on the entire array job completion
-sbatch --dependency=afterok:$ARRAY_JOB_ID summarize-inferences.sh
